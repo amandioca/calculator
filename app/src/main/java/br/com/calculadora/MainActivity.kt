22316eaccbc0498b033d1a734519b39bf8340860
am4ndioc4 <amandaamorim.ctt@gmail.com>
2024-04-13 21:24:42 -0300

    logic refactored

--- app/src/main/java/br/com/calculadora/MainActivity.kt
+++ app/src/main/java/br/com/calculadora/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import br.com.calculadora.databinding.ActivityMainBinding
@@ -12,97 +13,135 @@ import net.objecthunter.exp4j.operator.Operators
 
 
 class MainActivity : AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityMainBinding
-    private val PARENS = "parens"
+    private val parens = "( )"
+    private val parenOpen = '('
+    private val parenClose = ')'
+    private val firstOperatorAllowed = arrayOf("-", "%")
+    private val operators = charArrayOf('+', '-', '÷', '×', '%', '=')
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.num0.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}0"
+        val buttons = listOf(
+            binding.num0,
+            binding.num1,
+            binding.num2,
+            binding.num3,
+            binding.num4,
+            binding.num5,
+            binding.num6,
+            binding.num7,
+            binding.num8,
+            binding.num9,
+            binding.div,
+            binding.times,
+            binding.min,
+            binding.plus,
+            binding.percent,
+            binding.parens,
+            binding.comma,
+            binding.equal,
+            binding.backspace,
+            binding.allClear
+        )
+
+        for (button in buttons) {
+            button.setOnClickListener() {
+                val buttonText = when (button) {
+                    is Button -> button.text.toString()
+                    else -> ""
+                }
+
+                if (isInt(buttonText)) {
+                    binding.calc.text = binding.calc.text.toString().plus(buttonText)
+                } else {
+                    when {
+                        buttonText == "AC" -> {
+                            binding.calc.text = ""
+                            binding.result.text = ""
+                        }
+                        buttonText.isEmpty() -> backspace()
+                        buttonText == "=" && binding.calc.text.isNotEmpty() -> calculateResult()
+                        else -> symbolClickListener(button, buttonText)
+                    }
+                }
+            }
         }
-        binding.num1.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}1"
-        }
-        binding.num2.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}2"
-        }
-        binding.num3.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}3"
-        }
-        binding.num4.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}4"
-        }
-        binding.num5.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}5"
-        }
-        binding.num6.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}6"
-        }
-        binding.num7.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}7"
-        }
-        binding.num8.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}8"
-        }
-        binding.num9.setOnClickListener(){
-            binding.calc.text = "${binding.calc.text}9"
-        }
-        binding.plus.setOnClickListener(){
-            addSymbol("+")
-        }
-        binding.min.setOnClickListener(){
-            addSymbol("-")
-        }
-        binding.times.setOnClickListener(){
-            addSymbol("×")
-        }
-        binding.div.setOnClickListener(){
-            addSymbol("÷")
-        }
-        binding.parens.setOnClickListener(){
-            addSymbol(PARENS)
-        }
-        binding.percent.setOnClickListener(){
-        }
-        binding.comma.setOnClickListener(){
-            addSymbol(",")
-        }
-        binding.backspace.setOnClickListener(){
+    }
+
+    private fun isInt(char: String): Boolean {
+        return char.toIntOrNull() != null
+    }
+
+    private fun isOperator(char: Char): Boolean {
+        return operators.contains(char)
+    }
+
+    private fun backspace (){
+        if (binding.calc.text.isNotEmpty())
             binding.calc.text = binding.calc.text.dropLast(1)
-        }
-        binding.allClear.setOnClickListener(){
-            binding.calc.text = ""
-            binding.result.text = ""
-        }
-
-        binding.equal.setOnClickListener(){
-            val calc = binding.calc.text.toString().replace('÷','/').replace('×','*').replace(',', '.')
-            val expression = ExpressionBuilder(calc).build()
-            binding.result.text = formatResult(expression.evaluate())
-        }
-
+        else updateDisplayCalc("")
     }
 
-    private fun addSymbol(symbol: String) {
-        val calcText = binding.calc.text.toString()
+    private fun symbolClickListener(button: View, buttonText: String) {
+        var lastChar = ' '
+        var displayCalcText = ""
+        var displayFirstOperator = false
 
-        if (calcText.contains('(') && symbol == PARENS){
-            binding.calc.text = calcText.plus(')')
-        } else if (symbol == PARENS) {
-            binding.calc.text = calcText.plus('(')
-        } else if (Operator.isAllowedOperatorChar(calcText.last())) {
-            binding.calc.text = calcText.substring(0, calcText.length - 1).plus(symbol)
-        } else {
-            binding.calc.text = calcText.plus(symbol)
+        if (binding.calc.text.isNotEmpty()) {
+            displayCalcText = binding.calc.text.toString()
+            lastChar = binding.calc.text.last()
         }
 
+        firstOperatorAllowed.forEach { op ->
+            if (op == displayCalcText) displayFirstOperator = true
+        }
+        val countParentOpen = displayCalcText.count { it == parenOpen }
+        val countParentClose = displayCalcText.count { it == parenClose }
+
+        return if (displayCalcText.isEmpty() && firstOperatorAllowed.contains(buttonText)){
+            updateDisplayCalc(buttonText)
+        }
+        else {
+             if (buttonText == parens) {
+                if ((displayCalcText.contains(parenOpen) && isInt(lastChar.toString())) ||
+                    (lastChar == parenClose && countParentOpen > countParentClose)) {
+                    updateDisplayCalc(parenClose.toString())
+                } else {
+                    updateDisplayCalc(parenOpen.toString())
+                }
+            }
+            else if ((!displayFirstOperator || firstOperatorAllowed.contains(buttonText)) && (isOperator(lastChar) || lastChar.toString() == buttonText)) {
+                backspace()
+                updateDisplayCalc(buttonText)
+            }
+            else if (displayCalcText.isNotEmpty() && !displayFirstOperator) {
+                updateDisplayCalc(buttonText)
+            } else {
+                // No action needed, binding.calc.text remains the same
+            }
+        }
     }
 
-    private fun formatResult(value: Double): String {
+    private fun updateDisplayCalc(upCalc: String){
+        binding.calc.text = binding.calc.text.toString().plus(upCalc)
+    }
+
+    private fun calculateResult() {
+        val calc = binding.calc.text.toString()
+                            .replace('÷','/')
+                            .replace('×','*')
+                            .replace(',','.')
+
+        val expression = ExpressionBuilder(calc).build()
+        binding.result.text = formatResult(expression.evaluate())
+    }
+
+    private fun formatResult(value: Double): CharSequence? {
         val formatted = if (value % 1 == 0.0) {
             value.toInt().toString()
         } else {
@@ -110,6 +149,4 @@ class MainActivity : AppCompatActivity() {
         }
         return formatted.replace('.',',')
     }
-
-
 }
